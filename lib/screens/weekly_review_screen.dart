119424ca81c9dc1a2ceea9a9f88a6c103a49ffdf
zdmgold <zdmgold@gmail.com>
2026-08-06 08:00:26 +0100

    weekly_review_screen.dart

--- lib/screens/weekly_review_screen.dart
+++ lib/screens/weekly_review_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../core/models/scripture_verse.dart';
 import '../core/models/daily_entry.dart';
 import '../core/models/weekly_summary.dart';
 import '../core/providers/entries_provider.dart';
